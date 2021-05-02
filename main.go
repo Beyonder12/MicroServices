5a6aeb85ceb334070ee335630452c0e89eb23539
Fajr <fajri.illahi@medicaboo.com>
2021-05-02 11:47:53 +0700

    commit2

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-
-	})
-
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye!")
-	})
 	http.ListenAndServe(":9090", nil)
 }
