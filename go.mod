1c2f316ed10fa0663bf80514925c664890ba9b62
Fajr <fajri.illahi@medicaboo.com>
2021-05-02 12:29:46 +0700

    mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Beyonder12/MicroServices/product-api
+
+go 1.16
+
+require github.com/Beyonder12/env v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/Beyonder12/working/handlers"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
 	http.ListenAndServe(":9090", nil)
 	//this is micro21
 }
